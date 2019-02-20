530ba2984a8ae36a6f15d299bec5f591c51a615a
Parker Wokwicz <pwokwicz@parkers-macbook-air.local>
2019-02-20 11:15:16 -0600

    Initial Commit

---
+++ Top Stories/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Top Stories
+//
+//  Created by Parker Wokwicz on 2/20/19.
+//  Copyright © 2019 Parker Wokwicz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
