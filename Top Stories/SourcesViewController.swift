8fad47839c7275172df3c9361924b7dd91e0a69d
Parker Wokwicz <pwokwicz@parkers-macbook-air.local>
2019-02-20 11:37:59 -0600

    SwiftyJSON

--- Top Stories/SourcesViewController.swift
+++ Top Stories/SourcesViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class SourcesViewController: UITableViewController {
-
+    
+    var sources = [[String: String]]()
+    let apiKey = "5d892509a49046a087917c466fa80d09"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.title = "News Sources"
+        let query = "https://newsapi.org/v1/sources?language=en&country=us&apiKey=\(apiKey)"
     }
-
-
+    func parse(json: JSON) {
+        for result in json["sources"].arrayValue {
+            let id = result["id"].stringValue
+            let name = result["name"].stringValue
+            let description = result["description"].stringValue
+            let source = ["id": id, "name": name, "description": description]
+            
+        }
+    }
 }
 
